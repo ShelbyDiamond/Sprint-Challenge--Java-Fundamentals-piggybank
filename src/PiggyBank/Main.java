a430963ced359c6d28996a2f565c4b472150b6fa
Shelby Diamond <Shelbydiamond518@gmail.com>
2019-09-06 08:46:03 -0700

    added semi colons to debug
    made my package lowercase and that seemed to fix it

--- src/PiggyBank/Main.java
+++ src/PiggyBank/Main.java
@@ -1,4 +1,4 @@
-package PiggyBank;
+package piggybank;
 
 import java.util.*;
 
@@ -13,22 +13,24 @@ public class Main {
 
     ArrayList<AbstractMoney> myPiggy = new ArrayList<>();
     //quantities pulled from ReadMe:
-    myPiggy.add(new Quarters())
-    myPiggy.add(new Dimes())
-    myPiggy.add(new Dollars(5))
-    myPiggy.add(new Nickels(3))
-    myPiggy.add(new Dimes(7))
-    myPiggy.add(new Dollars())
-    myPiggy.add(new Pennies(10))
+    myPiggy.add(new Quarters());
+    myPiggy.add(new Dimes());
+    myPiggy.add(new Dollars(5));
+    myPiggy.add(new Nickels(3));
+    myPiggy.add(new Dimes(7));
+    myPiggy.add(new Dollars());
+    myPiggy.add(new Pennies(10));
 
     // counts the elements in the container
     for (int i = 0; i < myPiggy.size(); i++)
-    System.out.println(myPiggy.get(i).toString())
+    System.out.println(myPiggy.get(i).toString());
 
     //adds up the sum of the elements
     for (int i = 0; i < myPiggy.size(); i++)
     sum += myPiggy.get(i).getSum();
 
     //print a blank line
-    System.out.println()
+    System.out.println();
+
+    System.out.println("The piggy bank holds " + fp.format(sum));
 }
