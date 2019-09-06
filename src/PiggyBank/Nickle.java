dfb7f7758047a1a29d3b7cb46e965bd92bd81240
Shelby Diamond <Shelbydiamond518@gmail.com>
2019-09-06 09:15:54 -0700

    Thought it was done, but has 28 errors.
    Debugging now.

--- src/PiggyBank/Nickle.java
+++ src/PiggyBank/Nickle.java
@@ -3,7 +3,7 @@ package piggybank;
 public class Nickel extends AbstractMoney {
    
     public Nickel() {
-        super(0.05, 1, "Nickel")
+        super(0.05, 1, "Nickel");
     }
 
     public Nickel(int inventory) {
