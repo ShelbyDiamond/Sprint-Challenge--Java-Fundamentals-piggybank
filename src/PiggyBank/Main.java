0aaa93f46880bd29fefe7a8e84be2b86bfffa9f2
Shelby Diamond <Shelbydiamond518@gmail.com>
2019-09-06 09:40:19 -0700

    debugged, added ; and fixed spelling errors

--- src/PiggyBank/Main.java
+++ src/PiggyBank/Main.java
@@ -5,33 +5,33 @@ import java.text.DecimalFormat;
 
 public class Main {
 
-    public static void main(String[], args) {
-    // copy pasted from ReadMe:
-    DecimalFormat fp = new DecimalFormat("$###,###.00");
-    double sum = 0;
+    public static void main(String[] args) {
+        // copy pasted from ReadMe:
+        DecimalFormat fp = new DecimalFormat("$###,###.00");
+        double sum = 0;
 
-    ArrayList<AbstractMoney> myPiggy = new ArrayList<>();
+        ArrayList<AbstractMoney> myPiggy = new ArrayList<>();
 
-    //quantities pulled from ReadMe:
-    myPiggy.add(new Quarter());
-    myPiggy.add(new Dime());
-    myPiggy.add(new DollarBill(5));
-    myPiggy.add(new Nickel(3));
-    myPiggy.add(new Dime(7));
-    myPiggy.add(new DollarBill());
-    myPiggy.add(new Pennies(10));
+        // quantities pulled from ReadMe:
+        myPiggy.add(new Quarter());
+        myPiggy.add(new Dime());
+        myPiggy.add(new DollarBill(5));
+        myPiggy.add(new Nickel(3));
+        myPiggy.add(new Dime(7));
+        myPiggy.add(new DollarBill());
+        myPiggy.add(new Penny(10));
 
-    // counts the elements in the container
-    for (int i = 0; i < myPiggy.size(); i++)
-    System.out.println(myPiggy.get(i).toString());
+        // counts the elements in the container
+        for (int i = 0; i < myPiggy.size(); i++)
+            System.out.println(myPiggy.get(i).toString());
 
-    //adds up the sum of the elements
-    for (int i = 0; i < myPiggy.size(); i++)
-    sum += myPiggy.get(i).getSum();
+        // adds up the sum of the elements
+        for (int i = 0; i < myPiggy.size(); i++)
+            sum += myPiggy.get(i).getSum();
 
-    //print a blank line
-    System.out.println();
+        // print a blank line
+        System.out.println();
 
-    System.out.println("The piggy bank holds " + fp.format(sum));
-}
+        System.out.println("The piggy bank holds " + fp.format(sum));
+    }
 }
