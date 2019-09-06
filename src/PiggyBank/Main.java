c6ee53ca6070e03c2a4ce1b6a3f0301629551a46
Shelby Diamond <Shelbydiamond518@gmail.com>
2019-09-06 08:38:05 -0700

    created my file paths
    added code in Main.java. Has bugs, still isn't working.

---
+++ src/PiggyBank/Main.java
@@ -0,0 +1,34 @@
+package PiggyBank;
+
+import java.util.*;
+
+import jdk.javadoc.internal.doclets.formats.html.SourceToHTMLConverter;
+
+import java.text.DecimalFormat;
+
+public class Main {
+    // copy pasted from ReadMe:
+    DecimalFormat fp = new DecimalFormat("$###,###.00");
+    double sum = 0;
+
+    ArrayList<AbstractMoney> myPiggy = new ArrayList<>();
+    //quantities pulled from ReadMe:
+    myPiggy.add(new Quarters())
+    myPiggy.add(new Dimes())
+    myPiggy.add(new Dollars(5))
+    myPiggy.add(new Nickels(3))
+    myPiggy.add(new Dimes(7))
+    myPiggy.add(new Dollars())
+    myPiggy.add(new Pennies(10))
+
+    // counts the elements in the container
+    for (int i = 0; i < myPiggy.size(); i++)
+    System.out.println(myPiggy.get(i).toString())
+
+    //adds up the sum of the elements
+    for (int i = 0; i < myPiggy.size(); i++)
+    sum += myPiggy.get(i).getSum();
+
+    //print a blank line
+    System.out.println()
+}
